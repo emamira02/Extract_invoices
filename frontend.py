d0b14af0d8f83eb646fa599f4bffb9f14330790e
Emanuele Miranda <168436566+emamira02@users.noreply.github.com>
2025-03-28 16:30:11 +0100

    frontend ridefinito, aggiunta di varie lib, qualche piccola modifica estetica, aggiunta func per download con base64 e html, aggiunta form per un unico pulsante di submit e download

--- frontend.py
+++ frontend.py
@@ -4,12 +4,15 @@ import logging
 import pandas as pd
 import os
 import io
+from io import BytesIO
+import base64
 from PIL import Image
+import streamlit.components.v1 as components
 from backend import analyze_invoice
 
 # configuriamo la nostra pagina per visualizzare tutto centralmente, ed impostando il titolo
 st.set_page_config(
-    page_title="Receipt Extractor",
+    page_title="Data Extractor",
     layout="centered"
 )
 
@@ -48,7 +51,39 @@ else:
         logging.info(f"User {st.experimental_user.name} ({st.experimental_user.email}) successfully logged in.")
 
     # il titolo della nostra app con qualche edit estetico
-    st.markdown("# Extract :blue[Invoice Data] with :blue-background[Azure AI]")
+    st.markdown("# Extract :blue[Data] with :blue-background[Azure AI]")
+
+    def download_button(object_to_download, download_filename):
+        #con un try-except andiamo a gestire il download del file, se è un dataframe
+        #lo convertiamo in csv, altrimenti se è un byte lo codifichiamo in base64 e lo convertiamo in stringa
+        try:
+            if isinstance(object_to_download, pd.DataFrame):
+                object_to_download = object_to_download.to_csv(index=False)
+            elif isinstance(object_to_download, bytes):
+                #qui andiamo a codificare l'oggetto in base64 e lo convertiamo in stringa per il download, che verrà
+                #eseguito tramite un link html che verrà cliccato automaticamente per scaricare il file, altrimenti
+                #restituisce un errore di download del file con un messaggio di errore e un log di errore 
+                b64 = base64.b64encode(object_to_download).decode()
+            else:
+                object_to_download = json.dumps(object_to_download, indent=4, ensure_ascii=False)
+                b64 = base64.b64encode(object_to_download.encode()).decode()
+
+            dl_link = f"""
+            <html>
+            <head>
+            <title>Start Auto Download file</title>
+            <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+            <script>
+            $('<a href="data:application/json;base64,{b64}" download="{download_filename}">')[0].click()
+            </script>
+            </head>
+            </html>
+            """
+            return dl_link
+        except Exception as e:
+            logging.error(f"Errore nella creazione del link di download: {e}")
+            st.error(f"Errore nella creazione del link di download: {e}")
+            return None
 
 #la funzione per gestire il file che viene caricato, se non è vuota allora il file
 #viene letto, andando a verificare però che il file sia un file pdf, ed in caso creando
@@ -113,15 +148,6 @@ else:
 #per mostrare solo i dati in italiano creiamo un dizionario al quale aggiungeremo tutti i parametri
         data_it = {}
 
-        data_it["Nome Venditore"] = st.text_input("Nome Venditore", value=data.get("VendorName", "N/A"), key="vendor_name")
-        data_it["Indirizzo Venditore"] = st.text_input("Indirizzo Venditore", value=data.get("VendorAddress", "N/A"), key="vendor_address")
-        data_it["Numero di telefono Venditore"] = st.text_input("Numero di telefono Venditore", value=data.get("MerchantPhoneNumber", "N/A"), key="vendor_phone")
-        data_it["Data"] = st.text_input("Data", value=data.get("InvoiceDate", "N/A"), key="invoice_date")
-        data_it["Orario"] = st.text_input("Orario", value=data.get("TransactionTime", "N/A"), key="transaction_time") 
-        data_it["PIVA"] = st.text_input("PIVA", value=data.get("VendorTaxId", "N/A"), key="vendor_tax_id")
-        data_it["Totale"] = st.text_input("Totale", value=data.get("InvoiceTotal", "N/A"), key="invoice_total")
-      
-
         # questa è la nostra lista di prodotti in un dataframe, che in caso non ci sia nulla restituisci un dataframe vuoto
         # che può essere modificata
         st.subheader("Lista di Prodotti")
@@ -131,14 +157,56 @@ else:
             df = pd.DataFrame()
 
         edited_df = st.data_editor(df, num_rows="dynamic", key="items_df") 
+        lista_prodotti = edited_df.to_dict("records")
 
-        data_it["Lista Prodotti"] = edited_df.to_dict("records")
+        #creiamo un form per poter modificare i dati in italiano, con i parametri che andremo ad aggiornare
+        with st.form(key="edit_form"):
+            data_it["Nome Venditore"] = st.text_input("Nome Venditore", value=data.get("VendorName", "N/A"), key="vendor_name")
+            data_it["Indirizzo Venditore"] = st.text_input("Indirizzo Venditore", value=data.get("VendorAddress", "N/A"), key="vendor_address")
+            data_it["Numero di telefono Venditore"] = st.text_input("Numero di telefono Venditore", value=data.get("MerchantPhoneNumber", "N/A"), key="vendor_phone")
+            data_it["Data"] = st.text_input("Data", value=data.get("InvoiceDate", "N/A"), key="invoice_date")
+            data_it["Orario"] = st.text_input("Orario", value=data.get("TransactionTime", "N/A"), key="transaction_time")
+            data_it["PIVA"] = st.text_input("PIVA", value=data.get("VendorTaxId", "N/A"), key="vendor_tax_id")
+            data_it["Totale"] = st.text_input("Totale", value=data.get("InvoiceTotal", "N/A"), key="invoice_total")
+
+            submit_button = st.form_submit_button(label="Aggiorna e Scarica Dati")
+
+        if submit_button:
+            #aggiorniamo i dati in italiano e creiamo un file json con essi per il download 
+            json_data_italiano = {
+                "Nome Venditore": data_it["Nome Venditore"],
+                "Indirizzo Venditore": data_it["Indirizzo Venditore"],
+                "Numero di telefono Venditore": data_it["Numero di telefono Venditore"],
+                "Data": data_it["Data"],
+                "Orario": data_it["Orario"],
+                "PIVA": data_it["PIVA"],
+                "Totale": data_it["Totale"],
+                "Lista Prodotti": lista_prodotti
+            }
+        #usando un try-except per gestire eventuali errori, andiamo a creare un file json usando la libreria json e buffer
+        #che andremo a scrivere e scaricare, in caso di successo restituisce un messaggio di successo, altrimenti un errore
+        #relativo all'aggiornamento e download del file, restituisce i dati in italiano aggiornati 
+            try:
+                json_string = json.dumps(json_data_italiano, indent=4, ensure_ascii=False)
+                buff = BytesIO()
+                buff.write(json_string.encode('utf-8'))
+                buff.seek(0)
+
+                components.html(
+                    download_button(buff.getvalue(), f"{st.session_state['uploaded_file_name']}_italiano.json"),
+                    height=0,
+                )
+                st.success("Dati aggiornati e file JSON scaricato con successo!")
+
+            except Exception as e:
+                logging.error(f"Errore durante l'aggiornamento dei dati e il download: {e}")
+                st.error(f"Errore durante l'aggiornamento dei dati e il download: {e}")
 
         return data_it
 
     #usiamo la funzione di streamlit per caricare un file pdf e consentire solo quel formato
     uploaded_file = st.file_uploader(
-        label = "Upload a PDF Invoice File", 
+        label = "Upload an Invoice or a Receipt", 
         type=["pdf", "jpg","png", "jpeg"],
         key="file_uploader"
         )
@@ -147,8 +215,6 @@ else:
     #inizializziamo le variabili di sessione che andremo ad utilizzare
     if 'extracted_data' not in st.session_state:
         st.session_state['extracted_data'] = None
-    if 'edited_data' not in st.session_state:
-         st.session_state['edited_data'] = None
     if 'uploaded_file_name' not in st.session_state:
         st.session_state['uploaded_file_name'] = None
 
@@ -160,7 +226,6 @@ else:
         #riavviare l'analisi in caso di cambio file caricato, resettando i dati estratti
         if uploaded_file.name != st.session_state['uploaded_file_name']:
             st.session_state['extracted_data'] = None  
-            st.session_state['edited_data'] = None
             st.session_state['uploaded_file_name'] = uploaded_file.name
 
         st.success(f"File {uploaded_file.name} caricato con successo")
@@ -182,18 +247,6 @@ else:
             #modifica di essi, in caso contrario restituisce un errore di estrazione dati
             if st.session_state['extracted_data']:
                 st.header("Dati Estratti")
-                st.session_state['edited_data'] = edit_data(st.session_state['extracted_data'])
-
-               
-
-            #creiamo un bottone per scaricare i nostri dati in formato json
-                st.download_button(
-                    label="Scarica i dati in formato JSON",
-                    data=json.dumps(st.session_state['edited_data'], indent=4, ensure_ascii=False).encode('utf-8'),
-                    file_name=f"{uploaded_file.name}.json",
-                    mime="application/json",
-                    on_click= "ignore"
-                )
-                os.remove(temporary_file_path)  
+                edit_data(st.session_state['extracted_data'])
             else:
                 st.error("Impossibile estrarre i dati dal documento.")
